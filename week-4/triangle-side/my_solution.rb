ad76a18f54ece01dfd1deda61c3ec7869b8e8fe3
Marcel Galang <marcelgalang@gmail.com>
2015-09-11 09:05:07 -0400

    Add new .rb and spec files

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -1,30 +1,15 @@
-# I worked on this challenge [by myself, with: Kim Allen ].
+# I worked on this challenge with: Kim Allen .
 
 
 # Your Solution Below
 
-def valid_triangle?(a, b, c)
 
-if a==b && a==c
-  p true
-  puts "True, an equilateral triangle!"
-elsif
-  (a%3==0 || a%4==0 || a%5==0) && (b%3==0 || b%4==0 || b%5==0) && (c%3==0 || c%4==0 || c%5==0)
-  p true
-  puts "True!  This is a right triangle!"
-elsif (a+b > c) &&
-  (a+c > b) &&
-  (b+c > a)
-  p true
-  puts "this is a triangle"
-
-elsif (a==0 || b==0 ||c==0)
-  p false
-
-else
-  p false
-  puts "Sorry, this is not a triangle."
- end
+def valid_triangle?(a,b,c)
+  if a*b*c == 0
+    false
+  elsif (a+b > c) && (a+c > b) && (b+c > a)
+    true
+  end
 end
 
 
