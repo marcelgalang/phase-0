e8058933c41c9854f48f069f291ce9cbdbbdd2c5
Marcel Galang <marcelgalang@gmail.com>
2015-09-18 21:08:57 -0400

    Add soulition and reflection

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -78,3 +78,16 @@ p mode([1,2,3])
 
 
 # 4. Reflection
+=begin
+Which data structure did you and your pair decide to implement and why?
+Initially, we looked to solve in a hash but my pair realized that we needed to stay working with the array until the final steps.  We wanted to count the occurrences and compare the length before making the hash.
+
+Were you more successful breaking this problem down into implementable pseudocode than the last with a pair?
+I think it was better but there was still a giant hole in the section that would have steps to find the solution.  the set up was better though.
+
+What issues/successes did you run into when translating your pseudocode to code?
+Finding the correct words helped with using the right method.  This too some reconsideration in practice.
+
+What methods did you use to iterate through the content? Did you find any good ones when you were refactoring? Were they difficult to implement?
+The methods .count and .length were vital for comparisons.  The .collect made the hash conversion to array much easier.  Implementing was not hard.  Finding or thinking in terms of the methods that exist was more difficult.
+=end
