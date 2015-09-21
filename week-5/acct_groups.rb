9c1c7deaaa282899834117919b6e470da27d5120
Marcel Galang <marcelgalang@gmail.com>
2015-09-21 14:31:16 -0400

    Add reflection

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -1,4 +1,8 @@
-
+# In list of students
+# out 10 groups of 4-5 no less than 3, no more than 5
+# steps
+# -make an array of class list
+# -divide relatively evenly
 
 def arr_to_groups(num)
   arr=%("Kim Allen"
@@ -59,5 +63,19 @@ end
 
 arr_to_groups(4)
 
+=begin
 
+5.6
+What was the most interesting and most difficult part of this challenge?
+Getting the list to be cleaned up after grabbing it from the DBC page.  This is how I learned how line breaks are represented with (/"\n"/).
+Do you feel you are improving in your ability to write pseudocode and break the problem down?
+I do but not necessarily on this challenge as I saw it as only a few steps.
+Was your approach for automating this task a good solution? What could have made it even better?
+If I had the time I would have formatted the new arrays to read more easily.
+What data structure did you decide to store the accountability groups in and why?
+I used arrays as there only seemed to be one important value, the names.  If I were to add the city cohort or another piece of info to each student I may have used a hash.
+What did you learn in the process of refactoring your initial solution? Did you learn any new Ruby methods?
+I was trying to use #each and #slice.  In my research I found #each_slice and the solution seemed obvious.
+
+=end
 
