68df9c00b95d027677072612fae18056d6a86ffe
Marcel Galang <marcelgalang@gmail.com>
2015-10-01 16:11:00 -0400

    Add pseudo code

---
+++ week-7/group_project_solution.js
@@ -0,0 +1,39 @@
+// To Coder:  I can make the assumption all three functions will use the same original array but I still wrote them as separate ones.
+
+// SUM
+
+// Create a function named sum with an argument being the list of numbers
+// Create an empty array
+// Move the given list of numbers into the array.
+// . From that array, create a new array that is an odd length
+// Add up the odd length elements
+// From the original array, the remaining elements should create a new array that is an even length
+// Add up the even length elements
+
+
+//   --
+// MEAN
+
+// 1.Create a function named mean with an argument being an array
+
+// 2.from that array  create a new array with an odd number of elements
+// 3.divide that sum by number of elements to get the average of odd length array
+
+// 4.from the remainder of the original array sum the elements of this even length array
+// 5.divide that sum by number of elements to get the average of even length elements
+
+// --
+
+
+
+// MEDIAN
+
+// 1.Create a function called median that takes a list of numbers an argument
+// 2. Create an empty array
+// 3. Move the given list of numbers into the array.
+
+// 4. From the array create an array that is of odd length
+// 5. Divide the array by two
+// 6. Take the quotient and use it as an index number to find the middle (or closest to middle) number
+
+// 7. Repeat steps 4-6 for the even length array
